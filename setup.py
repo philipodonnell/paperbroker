0234df41bea71c96625772b79264937818b46bd2
odonnellphil <philip@scarboroughventures.com>
2017-07-10 22:51:57 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='paperbroker',
+      version='0.0.1',
+      description='PaperBroker',
+      author='Philip ODonnell',
+      author_email='philip@postral.com',
+      url='https://github.com/philipodonnell/paperbroker',
+      packages=['paperbroker'],
+     )
