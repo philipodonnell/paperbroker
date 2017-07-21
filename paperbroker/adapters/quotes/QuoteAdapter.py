cc70fb6d2c4f33c81cd5b510637e649a8f26ea47
odonnellphil <philip@scarboroughventures.com>
2017-07-20 21:22:07 -0400

    adding current date to QuoteAdapter

--- paperbroker/adapters/quotes/QuoteAdapter.py
+++ paperbroker/adapters/quotes/QuoteAdapter.py
@@ -1,6 +1,20 @@
-
+import arrow
 
 class QuoteAdapter:
+
+    def __init__(self, current_date=None):
+        self.set_current_date(current_date)
+
+    def set_current_date(self, current_date):
+        self._current_date = arrow.get(current_date).format('YYYY-MM-DD') if current_date is not None else None
+
+    def get_current_date(self):
+        return self._current_date if self._current_date else None
+
+    @property
+    def current_date(self):
+        return self.get_current_date()
+
     def get_quote(self, asset):
         raise NotImplementedError("QuoteAdapter.get_quote: You should subclass this and create an adapter.")
 
