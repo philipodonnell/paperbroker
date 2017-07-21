4bb8206ef0752528935b4bc3fccf35202a1716a1
odonnellphil <philip@scarboroughventures.com>
2017-07-20 21:25:23 -0400

    Added an assortment of properties to the position object to avoid having to do .asset and .quote everywhere.

--- paperbroker/positions.py
+++ paperbroker/positions.py
@@ -6,7 +6,7 @@
 
 from .quotes import Quote
 from .assets import asset_factory, Option
-
+from math import copysign
 
 class Position(object):
     """
@@ -21,34 +21,90 @@ class Position(object):
     so not in per asset amounts but in total dollar impact to cash.
     """
 
-    def __init__(self, asset, quantity: int, cost_basis: float=0.0, quote:Quote=None):
+    def __init__(self, asset, quantity: int, cost_basis: float=0.0, quote=None, position_id=None, open_date=None):
+        self.id = position_id or ('position' + str(id(self)))
+        self.open_date = open_date
         self.asset = asset_factory(asset)
         self.quantity = quantity
         self.cost_basis = cost_basis
         self.quote = quote
+        self.multiplier = 100 if isinstance(self.asset, Option) else 1
 
-    def get_total_cost_basis(self):
+    @property
+    def total_cost_basis(self):
         """
         :return: The total $ cost basis of the position
         """
-        multiplier = 100 if isinstance(self.asset, Option) else 1
-        return (self.cost_basis * abs(self.quantity)) * multiplier
+        return (self.cost_basis * abs(self.quantity)) * self.multiplier
 
-
-    def get_total_liquidation_value(self):
+    @property
+    def total_close_cost(self):
         """
-        :return: The total $ effect of liquidating the position. Negative means you gain by liquidating
+        :return: The cost to liquidate the position. Negative means you gain by liquidating
         """
         if self.quote is not None and self.quote.is_priceable():
-            multiplier = 100 if isinstance(self.asset, Option) else 1
-            return self.quote.price * self.quantity * multiplier * -1
+            return self.quote.price * self.quantity * self.multiplier * -1
         else:
             return 0.0
 
-    def get_total_profit(self):
+    @property
+    def close_cost(self):
+        """
+        :return: The cost to liquidate the position. Negative means you gain by liquidating
+        """
+        if self.quote is not None and self.quote.is_priceable():
+            return self.quote.price * copysign(1, self.quantity) * -1
+        else:
+            return 0.0
+
+    @property
+    def total_profit(self):
         """
         :return: The total $ profit you have unrealized in this position. Positive means you have profits, negative is losses
         """
-        return self.get_total_liquidation_value() - self.get_total_cost_basis()
+        return self.total_close_cost - self.total_cost_basis
 
 
+    @property
+    def profit(self):
+        """
+        :return: The total $ profit you have unrealized in this position. Positive means you have profits, negative is losses
+        """
+        return self.close_cost - self.cost_basis
+
+    @property
+    def strike(self): return self.asset.strike if isinstance(self.asset, Option) else None
+
+    @property
+    def expiration_date(self): return self.asset.expiration_date if isinstance(self.asset, Option) else None
+
+    @property
+    def underlying(self): return self.asset.underlying if isinstance(self.asset, Option) else None
+
+    @property
+    def option_type(self): return self.asset.option_type if isinstance(self.asset, Option) else None
+
+    @property
+    def symbol(self): return self.asset.symbol
+    @property
+    def asset_type(self): return self.asset.asset_type
+
+
+    @property
+    def delta(self): return (self.quote.delta * self.quantity * self.multiplier) if isinstance(self.asset, Option) else (1 * self.quantity * self.multiplier)
+    @property
+    def gamma(self): return (self.quote.gamma * self.quantity * self.multiplier) if isinstance(self.asset, Option) else (0.0)
+    @property
+    def theta(self): return (self.quote.theta * self.quantity * self.multiplier) if isinstance(self.asset, Option) else (0.0)
+    @property
+    def vega(self): return (self.quote.vega * self.quantity * self.multiplier) if isinstance(self.asset, Option) else (0.0)
+    @property
+    def rho(self): return (self.quote.rho * self.quantity * self.multiplier) if isinstance(self.asset, Option) else (0.0)
+    @property
+    def iv(self): return (self.quote.iv * self.quantity * self.multiplier) if isinstance(self.asset, Option) else (0.0)
+
+    @property
+    def total_price(self): return (self.quote.price * self.quantity * self.multiplier) if isinstance(self.asset, Option) and self.quote is not None else (0.0)
+
+    @property
+    def days_to_expiration(self): return self.quote.days_to_expiration if isinstance(self.asset, Option) and self.quote is not None else None
